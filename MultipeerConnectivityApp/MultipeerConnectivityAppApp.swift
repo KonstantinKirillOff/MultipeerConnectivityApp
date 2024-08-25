06538ea5c51587093e0ba188181a1c375b1c60ce
Konstantin Kirillov <kirilloffkostya@gmail.com>
2024-08-25 15:44:04 +0300

    Initial Commit

---
+++ MultipeerConnectivityApp/MultipeerConnectivityAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultipeerConnectivityAppApp.swift
+//  MultipeerConnectivityApp
+//
+//  Created by Konstantin Kirillov on 25.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct MultipeerConnectivityAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
